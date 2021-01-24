473f4a06faddb286de4bdc140b9fed91cae33220
Alien-Reborn <yashwanthgyash@gmail.com>
2021-01-24 23:07:10 +0530

    Assignment Updated

--- JAVA ASSIGNMENTS/Assignment 10/FileAssignment/src/fileoper/FileOperations.java
+++ JAVA ASSIGNMENTS/Assignment 10/FileAssignment/src/fileoper/FileOperations.java
@@ -24,6 +24,7 @@ public class FileOperations {
 		fw.close();
 		System.out.println("Input file data converted to Lower Case in target file");
 		
+		
 		// Convert all file information in upper case in target file. 
 		FileReader fr1 = new FileReader("C:\\Users\\YASHWANTH\\Desktop\\Zensar\\Zensar Java FS Training\\Java\\JAVA ASSIGNMENTS\\File10.txt");
 		FileWriter fw1 = new FileWriter("C:\\Users\\YASHWANTH\\Desktop\\Zensar\\Zensar Java FS Training\\Java\\JAVA ASSIGNMENTS\\UpperCase.txt");
@@ -39,20 +40,26 @@ public class FileOperations {
 		fw1.close();
 		System.out.println("Input file data converted to Upper Case in target file");
 		
-		// Convert all sentence first letter in upper case.
-		/* FileReader fr2 = new FileReader("C:\\Users\\YASHWANTH\\Desktop\\Zensar\\Zensar Java FS Training\\Java\\JAVA ASSIGNMENTS\\File10.txt");
-		BufferedReader br2 = new BufferedReader(fr2);
-		String line;
-		String out=" ";
-        	while ((line = br2.readLine()) !=null) {
-        		String words[]=line.split(" ");
-        		for(String word:words)
-          	    out  =out+Character.toUpperCase(word.charAt(0)) + word.substring(1) + " ";
-		}
-         	br2.close();
-		fr2.close();
-		System.out.println(out); */
 		
+		// Convert all sentence first letter in upper case.
+		FileReader fr2 = new FileReader("C:\\Users\\YASHWANTH\\Desktop\\Zensar\\Zensar Java FS Training\\Java\\JAVA ASSIGNMENTS\\File10.txt");
+		FileWriter fw2 = new FileWriter("C:\\Users\\YASHWANTH\\Desktop\\Zensar\\Zensar Java FS Training\\Java\\JAVA ASSIGNMENTS\\FirstLetterUpper.txt");
+		BufferedReader br2 = new BufferedReader(fr2);
+		
+		String line = null;
+		    System.out.println("Converting first letter of sentence to Upper Case in Target File");
+		    if((line = br2.readLine()) != null){
+		    	
+			        fw2.write(line.substring(0, 1).toUpperCase()+line.substring(1));
+		    }
+		    else{
+		    	 fw2.write(line.substring(0, 1).toUpperCase()+line.substring(1));
+		    }
+		    fw2.close();
+		    br2.close();
+		    fr2.close();
+	
+		    
 		// Display number of character present in file. 
 		FileReader fr3 = new FileReader("C:\\Users\\YASHWANTH\\Desktop\\Zensar\\Zensar Java FS Training\\Java\\JAVA ASSIGNMENTS\\File10.txt");
 		BufferedReader br3 = new BufferedReader(fr3);
@@ -63,9 +70,10 @@ public class FileOperations {
         		for(String word:words)
           	        count=count+word.length();
 		}
-         	br3.close();
+        br3.close();
 		fr3.close();
 		System.out.println("Number of characters present in file = "+count);
+		
 		
 		// Display number of words present in file.
 		FileReader fr4 = new FileReader("C:\\Users\\YASHWANTH\\Desktop\\Zensar\\Zensar Java FS Training\\Java\\JAVA ASSIGNMENTS\\File10.txt");
@@ -76,9 +84,7 @@ public class FileOperations {
           		String words[]=line2.split(" ");
           		count1=count1+words.length;
 		}
-         	fr4.close();
+        fr4.close();
 		System.out.println("Number of Words Present in File = "+count1);
-		
 	}
-
 }
