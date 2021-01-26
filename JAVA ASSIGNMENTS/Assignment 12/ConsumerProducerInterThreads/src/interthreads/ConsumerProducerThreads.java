ed8fe7e36a66b98943f0cbbfb93fa169b1797f69
Alien-Reborn <yashwanthgyash@gmail.com>
2021-01-26 21:38:21 +0530

    Assignment Updated

---
+++ JAVA ASSIGNMENTS/Assignment 12/ConsumerProducerInterThreads/src/interthreads/ConsumerProducerThreads.java
@@ -0,0 +1,102 @@
+package interthreads;
+
+class InterThread{
+	int n;
+	boolean value=false;
+	
+	public synchronized void put(int n) 
+	{
+	   while(value) {
+		   try {
+			   wait();
+		   }
+		   catch(Exception e) {
+			   
+		   }
+	   }
+	   System.out.println("Deposit Put : "+ n + " Consumer");
+	   this.n=n;
+	   value=true;
+	   notify();
+	}
+	
+	public synchronized void get()
+	{
+		while(!value)
+		{
+			try {
+				wait();
+			}
+			catch(Exception e) {
+				
+			}
+		}
+		System.out.println("Withdraw get : "+ n + " Producer");
+		value=false;
+		notify();
+	}
+}
+
+class Consumer implements Runnable
+{
+	InterThread cp;
+	
+	public Consumer(InterThread cp)
+	{
+		this.cp=cp;
+		Thread t=new Thread(this,"Consumer");
+		t.start();
+	}
+	
+	public void run() {
+		while(true)
+		{
+			cp.get();
+			try {
+				Thread.sleep(500);
+			}
+			catch(Exception e) {
+				
+			}
+		}
+	}
+}
+
+
+class Producer implements Runnable
+{
+	InterThread cp;
+	
+	public Producer(InterThread cp)
+	{
+		this.cp=cp;
+		Thread t=new Thread(this,"Producer");
+		t.start();
+	}
+	
+	public void run() {
+		int i=0;
+		while(true)
+		{
+			cp.put(i++);
+			try {
+				Thread.sleep(500);
+			}
+			catch(Exception e) {
+				
+			}
+		}
+	}
+}
+
+
+public class ConsumerProducerThreads {
+
+	public static void main(String[] args) {
+		InterThread cp=new InterThread();
+		new Producer(cp);
+		new Consumer(cp);
+		
+	}
+
+}
